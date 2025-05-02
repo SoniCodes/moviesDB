5b7ac0098de127762aa4685e279fd84c29217ade
SoniVrajCMIS202 <w1305696@myfcc.frederick.edu>
2025-05-02 11:25:19 -0400

    added directors sql script

--- moviesDB/movies_ddl.sql
+++ moviesDB/movies_ddl.sql
@@ -11,6 +11,10 @@ CREATE TABLE movie_db.actors (
     actor_id int NOT NULL PRIMARY KEY,
     first_name varchar(50),
     last_name varchar(50),
-    )
+)
 
-
+CREATE TABLE `directors` (
+    `director_id` int NOT NULL PRIMARY KEY,
+    `first_name` varchar(50),
+    `last_name` varchar(50),
+)
